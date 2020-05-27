aeebbf962d99032fcb7376cc45d75bb9a1b64960
Kushal Kumar Shrivastava <kushalchitransh@yahoo.in>
2020-05-28 01:14:02 +0530

    Commited the Updated screenhot method in Testing branch

--- Salesforce/src/main/java/com/salesforce/automaton/Base.java
+++ Salesforce/src/main/java/com/salesforce/automaton/Base.java
@@ -2,14 +2,19 @@ package com.salesforce.automaton;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -203,8 +208,9 @@ public class Base
 		return type + getDate();
 	}
 	
-	public static void takeScreenShot()
+	public static void takeScreenShot( WebDriver driver ) throws IOException
 	{
-		
+		File screenshot = ( (TakesScreenshot) driver ).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(screenshot, new File("/Screenshots"));
 	}
 }
